eb859459d7754b8521f67940bc21134387cfac1f
Sebastian Jensen <111466887+HEXtriple@users.noreply.github.com>
2024-01-29 10:48:01 +0100

    kommentarer

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// Importera nödvändiga moduler
 var http = require('http');
 var url = require('url');
 var mysql = require('mysql');
 var querystring = require('querystring');
 
+// Skapa en anslutning till MySQL-databasen
+// verkligen inte säkert
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -11,16 +14,18 @@ const db = mysql.createConnection({
   port: 3306
 });
 
-
+// Anslut till databasen
 db.connect();
 
-
+// Skapa en HTTP-server
 const server = http.createServer((req, res) => {
+  // Om förfrågan är en POST-förfrågan och sökvägen är '/chatbot'
   if (req.method === 'POST' && url.parse(req.url).pathname === '/chatbot') {
     let body = '';
     req.on('data', chunk => {
       body += chunk.toString();
     });
+    // Här tar servern emot data från förfrågan, sql databasen chatbot med tabellen messages
     req.on('end', () => {
       const post = querystring.parse(body);
       const question = post.message;
