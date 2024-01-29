c1bb03943e4452f70c29911877b46042f025a5af
Sebastian Jensen <111466887+HEXtriple@users.noreply.github.com>
2024-01-29 10:23:39 +0100

    Refactor client and server code

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+var http = require('http');
+var url = require('url');
+var mysql = require('mysql');
+var querystring = require('querystring');
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'chatbot'
+  database: 'chatbot',
+  port: 3306
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to database');
+
+db.connect();
+
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && url.parse(req.url).pathname === '/chatbot') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const post = querystring.parse(body);
+      const question = post.message;
+      db.query('SELECT response FROM messages WHERE question = ?', [question], (error, results, fields) => {
+        if (error) throw error;
+        res.end(results[0].response);
+      });
+    });
+  }
 });
+
+server.listen(3000); //Standarport är redan använd av något annat program, därför väljer vi 3000 istället
